b83ca4828ef0d17ee6b79b6e9ab0415ddbf4f4a2
OXXXYZED <execode12@gmail.com>
2025-05-08 12:21:12 +0500

    Update roles.py

--- data/roles.py
+++ data/roles.py
@@ -1,7 +1,7 @@
 ROLE_PRIORITY = {
     "lead": 5,
     "caller": 4,
-    "Capter 3 lvl": 3,
-    "Capter 2 lvl": 2,
-    "Capter 1 lvl": 1
+    "capter 3 lvl": 3,
+    "capter 2 lvl": 2,
+    "capter 1 lvl": 1
 }
